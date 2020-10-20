2f16390cb7e451bef21079d9fe337054d992071a
Dishebh Bhayana <dishebh27@gmail.com>
2020-10-20 20:52:51 +0530

    added basic setup of redux

---
+++ client/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route exact path="/surveys/new" component={SurveyNew} />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
