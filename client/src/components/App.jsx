f10656d71855a6943ecf41e23908fa44fc201d6e
Dishebh Bhayana <dishebh27@gmail.com>
2020-10-24 13:30:02 +0530

    added reduxForm

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,8 +7,7 @@ import { fetchUser } from "../actions";
 import Header from "./Header";
 import { Landing } from "./Landing";
 import { Dashboard } from "./Dashboard";
-
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import { SurveyNew } from "./surveys/SurveyNew";
 
 const App = ({ fetchUser }) => {
   useEffect(() => {
---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SurveyNew = () => {
+    return (
+        <div>
+            SurveyNew!
+        </div>
+    )
+}
