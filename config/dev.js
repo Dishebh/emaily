70c2fe189b544408597d1a677aedd5a3a6fcf73a
Dishebh Bhayana <dishebh27@gmail.com>
2020-10-30 21:45:58 +0530

    added config folder

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendgridApiKey: process.env.SENDGRID_API_KEY,
+  redirectDomain: process.env.REDIRECT_DOMAIN,
+};
