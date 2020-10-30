ec06de92b122d4f9038b7d43d9761b26d7494a54
Dishebh Bhayana <dishebh27@gmail.com>
2020-10-30 21:21:06 +0530

    added static routes in production

--- index.js
+++ index.js
@@ -40,6 +40,19 @@ app.use("/", require("./routes/authRoutes"));
 app.use("/api/stripe", require("./routes/billingRoutes"));
 app.use("/api/surveys", require("./routes/surveyRoutes"));
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file or main.css file
+  app.use(express.static('client/build'))
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = keys.PORT || 5000;
 
 app.listen(PORT, () => {
