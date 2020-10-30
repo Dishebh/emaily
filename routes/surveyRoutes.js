2f352aa49e1316efcade2c7b1c4dc19181783b73
Dishebh Bhayana <dishebh27@gmail.com>
2020-10-30 16:53:08 +0530

    added webhooks functionality to update Survey collection as per the data recieved

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,18 +11,43 @@ const router = express.Router();
 
 sgMail.setApiKey(keys.sendgridApiKey);
 
-router.get("/thanks", (req, res) => {
+router.get("/:surveyId/:choice", (req, res) => {
   res.send("Thanks for voting!");
 });
 
 router.post("/webhooks", (req, res) => {
   console.log("webhooks body: ", req.body);
 
-  const events = _.map(req.body, (event) => {
-    const pathname = new URL(event.url).pathname;
-    const p = new Path("/api/surveys/:surveyId/:choice");
-    console.log(p.test(pathname));
-  });
+  const p = new Path("/api/surveys/:surveyId/:choice");
+
+  const events = _.chain(req.body)
+    .map(({ email, url }) => {
+      const match = p.test(new URL(url).pathname);
+
+      if (match) {
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+    })
+    .compact()
+    .uniqBy('email', 'surveyId')
+    .each(({ surveyId, email, choice }) => {
+      Survey.updateOne(
+        {
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email: email, responded: false }
+          }
+        },
+        {
+          $inc: { [choice]: 1 },
+          $set: { 'recipients.$.responded': true },
+          lastResponded: new Date()
+        }
+      ).exec();
+    })
+    .value()
+
+  console.log('events: ', events);
 
   res.send("webhook called!");
 });
