55ba746c08615c10262cb45f15c6ee1830097d51
Dishebh Bhayana <dishebh27@gmail.com>
2020-10-30 21:41:32 +0530

    added morgan in development NODE_ENV

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const morgan = require("morgan");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const bodyParser = require("body-parser");
@@ -22,7 +21,10 @@ mongoose
 
 app.use(bodyParser.json());
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  const morgan = require("morgan");
+  app.use(morgan("dev"));
+}
 
 passportServices(keys, User, GoogleStrategy, passport);
 
